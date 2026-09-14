c93e6041914d5371d23375fbdd49b28848094d89
GKT01 <goktugdal34@gmail.com>
2026-09-14 16:19:35 +0300

    Simplified endpoints

--- GStore/Program.cs
+++ GStore/Program.cs
@@ -1,80 +1,15 @@
+using GameStore.Api.EndPoints;
 using GStore.Api.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-const string GetGameEndpointName = "GetGame";
 
-// small fake db for now
-List<GameDto> games = [
-    new (1, "Street Fighter II", "Fighting", 19.99M, new DateOnly(1992,7,15)),
-    new (2, "Final Fantasy VII Rebirth", "RPG", 69.99M, new DateOnly(2024,2,29)),
-    new (3 , "Astro Bot", "Platformer", 59.99M, new DateOnly(2024,9,6))
-];
-
-// GET /games
-app.MapGet("/games", () => games);
-
-
-
-// GET /games/1
-app.MapGet("/games/{id}", (int id) => {
-    var game = games.Find(game => game.Id == id);
-
-
-    return game is null ? Results.NotFound() : Results.Ok(game);
-}).WithName(GetGameEndpointName);
-
-// GET general
-app.MapGet("/", () => "Hello World!");
-
-// POST /games
-app.MapPost("/games",(CreateGameDto newGame) =>
-{
-   GameDto game = new(
-        games.Count + 1,
-        newGame.Name,
-        newGame.Genre,
-        newGame.Price,
-        newGame.ReleaseDate
-   );
-
-   games.Add(game);
-
-   return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game); 
-});
-
-
-// PUT /games/1
-app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
-{
-    var index = games.FindIndex(game => game.Id == id);
-    
-    if(index == -1)
-    {
-        return Results.NotFound();
-    }
-
-    games[index] = new GameDto(
-        id,
-        updatedGame.Name,
-        updatedGame.Genre,
-        updatedGame.Price,
-        updatedGame.ReleaseDate
-    );
-
-    return Results.NoContent();
-});
-
-// DELETE /games/1
-// no need for dto
-app.MapDelete("/games/{id}", (int id) =>
-{
-    games.RemoveAll(game => game.Id == id);
-
-    return Results.NoContent();
-});
+app.MapGameEndPoints();
 
 
 //html routes
 app.Run();
+
+// GET general
+        //group.MapGet("/", () => "Hello World!");
